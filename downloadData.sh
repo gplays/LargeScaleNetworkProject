ea67668f43184373b9cf840c46444063e006f38e
plays <guillaume.plays@gmail.com>
2018-03-26 14:02:35 +0200

    Adding helping files

---
+++ downloadData.sh
@@ -0,0 +1,8 @@
+mkdir data
+cd data
+wget https://static.aminer.org/lab-datasets/citation/dblp.v10.zip
+sudo apt-get install unzip
+unzip dblp.v10.zip
+wget https://static.aminer.org/lab-datasets/citation/citation-acm-v8.txt.tgz
+tar -xzf citation-acm-v8.txt.tgz
+rm dblp.v10.zip citation-acm-v8.txt.tgz
